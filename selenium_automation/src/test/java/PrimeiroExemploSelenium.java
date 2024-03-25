1ae24babce5fcab5f586b99eb22ab7b77b7ea355
Adnyslougansilva <adnyslougan@gmail.com>
2024-03-25 20:51:24 -0300

    Automacao formulario demo tests

--- selenium_automation/src/test/java/PrimeiroExemploSelenium.java
+++ selenium_automation/src/test/java/PrimeiroExemploSelenium.java
@@ -1,13 +1,17 @@
 import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
 public class PrimeiroExemploSelenium {
-    public static void main(String[] args) {
+
+    @Test
+    public void testSelenium () throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.automationtesting.in/Register.html");
+        Thread.sleep(5000);
         driver.manage().window().maximize();
 
         driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Adnys");
@@ -15,14 +19,10 @@ public class PrimeiroExemploSelenium {
         driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("Rua: Rio Grande do Sul n399 Nova Olimpia - pr");
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("qaautomatizando@test.com.br");
         driver.findElement(By.xpath("//input[@ng-model='Phone']")).sendKeys("1234567890");
-
         driver.findElement(By.xpath("//input[@value='Male']")).click();
-        driver.findElement(By.xpath("//input[@value='Male']")).isSelected();
-
         driver.findElement(By.xpath("//input[@id='checkbox2']")).click();
-        driver.findElement(By.xpath("//input[@id='checkbox2']")).isSelected();
-
         driver.findElement(By.id("msdd")).click();
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[contains(text(),'Portuguese')]")).click();
         driver.findElement(By.xpath("//*[contains(text(),'Hobbies')]")).click();
         driver.findElement(By.id("Skills")).click();
@@ -38,6 +38,9 @@ public class PrimeiroExemploSelenium {
         driver.findElement(By.id("firstpassword")).sendKeys("000111222");
         driver.findElement(By.id("secondpassword")).sendKeys("000111222");
         driver.findElement(By.xpath("//button[@id='submitbtn']")).click();
+
+        Thread.sleep(10000);
+        driver.quit();
     }
 
 }
